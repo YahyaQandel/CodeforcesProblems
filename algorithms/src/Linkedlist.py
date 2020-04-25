7c154f90c8b1b11dc4327600ace3f49f91bd2f87
YahyaQandel <yahya.qandel@gmail.com>
2020-04-25 03:19:54 +0200

    add linkedin

--- algorithms/src/Linkedlist.py
+++ algorithms/src/Linkedlist.py
@@ -43,7 +43,6 @@ class LinkedList:
         return current_node.value
 
     def delete(self, value):
-
         if not self.head:
             return None
 
@@ -72,6 +71,103 @@ class LinkedList:
             current_node = current_node.next
 
         return size
+
+    def reverse(self):
+        if not self.head.next:
+            return
+        current_node = prev = self.head
+        current_node = current_node.next
+        prev.next = None
+        while current_node.next:
+            temp = current_node.next
+            current_node.next = prev
+            prev = current_node
+            current_node = temp
+
+        current_node.next = prev
+        self.head = current_node
+
+    def reverse_list(self, l):
+        if not l:
+            return
+        current_node = prev = l
+        current_node = current_node.next
+        prev.next = None
+        while current_node.next:
+            temp = current_node.next
+            current_node.next = prev
+            prev = current_node
+            current_node = temp
+
+        current_node.next = prev
+        l = current_node
+        return l
+
+    def merge_list(self, h2):
+        current_node = self.head
+
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = h2.head
+
+    def sort(self):
+        end = None
+        while end != self.head:
+            p = self.head
+            while p.next != end:
+                q = p.next
+                if p.value > q.value:
+                    p.value, q.value = q.value, p.value
+                p = p.next
+            end = p
+
+    def is_palindrome(self):
+        cp_linked = self.copy_list(self.head)
+        c = self.reverse_list(cp_linked)
+        current_node = self.head
+
+        while current_node.next:
+            if c.value != current_node.value:
+                return False
+            c = c.next
+            current_node = current_node.next
+
+        return True
+
+    def copy(self):
+        current_node = self.head.next
+        cp = Node(self.head.value)
+        cpt = cp
+        while current_node:
+            temp = Node(current_node.value)
+            cpt.next = temp
+            cpt = cpt.next
+            current_node = current_node.next
+
+        return cp
+
+    def copy_list(self, l):
+        current_node = l
+        cp = Node(l.value)
+        cpt = cp
+        while current_node:
+            temp = Node(current_node.value)
+            cpt.next = temp
+            cpt = cpt.next
+            current_node = current_node.next
+
+        return cp
+
+    def __str__(self):
+        current_node = self.head
+        while current_node:
+            if current_node.next:
+                print("{} =>".format(current_node.value), end=" ")
+            else:
+                print("{}".format(current_node.value), end=" ")
+            current_node = current_node.next
+        return ""
 
 
 class Node:
--- algorithms/tests/test_linkedlist.py
+++ algorithms/tests/test_linkedlist.py
@@ -53,6 +53,56 @@ class LinkedInTest(unittest.TestCase):
         self.linkedlist.add_front(20)
         self.assertEqual(self.linkedlist.size(), 4)
 
+    def testReverse(self):
+        self.linkedlist.add_back(1)
+        self.linkedlist.add_back(2)
+        self.linkedlist.add_back(3)
+        self.linkedlist.add_back(4)
+        self.linkedlist.add_back(5)
+        # print(self.linkedlist)
+        self.linkedlist.reverse()
+        print(self.linkedlist)
+        self.assertEqual(self.linkedlist.get_first(), 5)
+
+    def testReverseEdgeCaseWithOneElement(self):
+        self.linkedlist.add_back(1)
+        # print(self.linkedlist)
+        self.linkedlist.reverse()
+        print(self.linkedlist)
+        self.assertEqual(self.linkedlist.get_first(), 1)
+
+    def testMergeTwoLists(self):
+        self.linkedlist.add_back(1)
+        self.linkedlist.add_back(2)
+        self.linkedlist.add_back(3)
+
+        self.linkedlist2 = LinkedList()
+        self.linkedlist2.add_back(4)
+        self.linkedlist2.add_back(5)
+        self.linkedlist2.add_back(6)
+        self.linkedlist.merge_list(self.linkedlist2)
+        self.assertEqual(self.linkedlist.get_first(), 1)
+        self.assertEqual(self.linkedlist.get_last(), 6)
+
+    def testSort(self):
+        self.linkedlist.add_back(6)
+        self.linkedlist.add_back(3)
+        self.linkedlist.add_back(5)
+        self.linkedlist.add_back(10)
+        self.linkedlist.add_back(2)
+        self.linkedlist.sort()
+        self.assertEqual(self.linkedlist.get_first(), 2)
+        self.assertEqual(self.linkedlist.get_last(), 10)
+
+    def testPalindrome(self):
+        self.linkedlist.add_back(1)
+        self.linkedlist.add_back(2)
+        self.linkedlist.add_back(3)
+        self.linkedlist.add_back(2)
+        self.linkedlist.add_back(1)
+        self.assertTrue(self.linkedlist.is_palindrome())
+
+
 
 def tearDown(self):
     self.linkedlist = None
